9304183d5b870705a7ac14fc505c3ab075a84588
unknown <atamossat@gmail.com>
2025-04-22 01:46:04 +0100

    done

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
                 "resources/js/app.js",
                 "resources/css/style.css",
             ],
-            refresh: true
+            refresh: true,
         }),
         tailwindcss(),
     ],
